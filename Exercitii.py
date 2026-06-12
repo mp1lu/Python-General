f2a164bf7fbc07dc36f19e4e92d695798cedfcb7
mp1lu <mihai.muntean@devnet.ro>
2026-06-12 10:28:05 +0300

    Exercitii

---
+++ Exercitii.py
@@ -0,0 +1,69 @@
+#Count vowels in a string: Given a string like "hello world", count how many vowels (a, e, i, o, u) it contains.
+# vowels=['a','e','i','o','u']
+# ent=input("enter a word").lower()
+# total=0
+# i=0
+# while i<len(ent):
+#     if ent[i] in vowels:
+#         total=total+1
+#     i=i+1
+# print(total)
+
+
+
+#Reverse a list without using .reverse() or slicing. Given [1, 2, 3, 4, 5], produce [5, 4, 3, 2, 1] using a loop.
+# text=[1,2,3,4,5,6]
+# lista=[]
+# i=len(text)-1
+# while i>=0:
+#     lista.append(text[i])
+#     i=i-1
+# print(lista)
+
+#Find the largest number in a list Given [3, 7, 1, 9, 4], find the max without using max()
+# text=[3, 7, 1, 9, 4]
+# i=0
+# max=text[0]
+# res=text[0]
+# while i<len(text):
+#     if text[i]>max:
+#         max=text[i]
+#     if text[i]<res:
+#         res=text[i]
+#     i=i+1
+# print('Maxim egal cu',max)
+# print('Minim egal cu',res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
